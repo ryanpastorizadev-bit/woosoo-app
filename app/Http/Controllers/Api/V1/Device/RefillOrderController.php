65e6a819d2f443e9b17ee4ef226669760bd9dfc9
Ryan Pastoriza <ryanpastoriza.dev@gmail.com>
2026-05-08 22:52:33 +0800

    Scope refill orders to bearer device

--- app/Http/Controllers/Api/V1/Device/RefillOrderController.php
+++ app/Http/Controllers/Api/V1/Device/RefillOrderController.php
@@ -6,12 +6,21 @@ use App\Actions\DeviceOrders\RefillOrderAction;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Device\RefillOrderRequest;
 use App\Http\Resources\DeviceOrderResource;
+use App\Models\Device;
 use App\Models\DeviceOrder;
+use Illuminate\Http\Exceptions\HttpResponseException;
 
 class RefillOrderController extends Controller
 {
     public function store(RefillOrderRequest $request, DeviceOrder $order, RefillOrderAction $action): DeviceOrderResource
     {
+        /** @var Device $device */
+        $device = $request->attributes->get('device');
+
+        if ((int) $order->device_id !== (int) $device->id) {
+            throw new HttpResponseException(response()->json(['message' => 'Order not found.'], 404));
+        }
+
         return DeviceOrderResource::make($action->execute($order, $request->validated()));
     }
 }
